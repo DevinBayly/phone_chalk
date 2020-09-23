be72966009416ae3d317d4523b7d4877af321897
DevinBayly <baylyd@email.arizona.edu>
2020-09-23 03:21:56 +0000

    build commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script>
-	//import Camera from "./camera.svelte"
-	import Test from "./test.svelte"
+	import Camera from "./camera.svelte"
+	// import Test from "./test.svelte"
 </script>
-<!-- <Camera></Camera> -->
-<Test></Test>
+<Camera></Camera>
--- src/canvas.svelte
+++ src/canvas.svelte
@@ -1,20 +1,53 @@
 <script>
-  let canvas,ocanvas,octx, ctx, img;
-  ocanvas = document.createElement("canvas")
-  let markstate = "mark-off";
-  let swap = false;
-  let GA = 0.1;
-  let filterstate = false;
-  let slowstate = "slo-mo";
-  let gridNum = 10;
+  export let cw,ch
+  let canvas;
+  let editor = (() => ({
+    on: false,
+    active() {
+      return this.on;
+    },
+    run(x,y) {
+      if (markstate == "mark-on") {
+        this.mark(x,y)
+      }
+    },
+    mark(x, y) {
+      let bbox = canvas.getBoundingClientRect()
+      octx.fillStyle = "red";
+      x = (x)*(img.width/canvas.width)*zoom  + sx - bbox.left
+      y = y*(img.height/canvas.height)*(img.width/img.height)*zoom+sy -bbox.top
+      octx.fillRect(x, y, 5, 5);
+      img2.src = offscreen.toDataURL();
+    }
+  }))();
+  let offscreen;
+  let octx;
+  let gridNum;
+  let GA = 1;
+  let ctx;
+  let slowstate = 0;
   let slow = 1;
-  let zoom = 1;
-  let sx = 0;
-  let sy = 0;
-  let maxX = 0;
-  let maxY = 0;
+  let markstate = "mark-off";
+  let img = new Image();
   let img2 = new Image();
+  import { onMount } from "svelte";
+  let sx = 0,
+    sy = 0,
+    zoom = 1;
+  let markFunc = () => {
+    if (markstate == "mark-on") {
+      markstate = "mark-off";
+    } else {
+      markstate = "mark-on";
+    }
+  };
+  let reset = () => {
+    // redraw on offcanvas and then reload the img2 and re issue draw canvas
+    octx.drawImage(img, 0, 0);
+    img2.src = offscreen.toDataURL();
+  };
   let drawCanvas = () => {
+    if (canvas == undefined) return;
     let finalwidth, finalheight;
     if (img.height > img.width) {
       finalwidth = (canvas.width * img.width) / img.height;
@@ -23,6 +56,8 @@
       finalwidth = canvas.width;
       finalheight = (canvas.height * img.height) / img.width;
     }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.globalAlpha = GA
     ctx.drawImage(
       img2,
       sx,
@@ -35,42 +70,31 @@
       finalheight
     );
   };
-  export let ch, cw;
-  import { onMount } from "svelte";
   onMount(() => {
-     canvas.height = ch;
-     canvas.width = cw;
-  });
-  let handleFiles = e => {
-    let URL = window.webkitURL || window.URL;
+    offscreen = document.createElement("canvas");
     ctx = canvas.getContext("2d");
-    img = new Image();
+    octx = offscreen.getContext("2d");
     img.onload = () => {
-      maxX = img.width;
-      maxY = img.height;
-
-    canvas.width = cw;
-    canvas.height = ch;
-    ctx.globalAlpha = GA;
-    ctx.drawImage(
-      img,
-      sx,
-      sy,
-      img.width * zoom,
-      img.height * zoom,
-      0,
-      0,
-      (img.width * canvas.height) / img.height,
-      canvas.height
-    );
-      img2.src = canvas.toDataURL();
+      offscreen.height = img.height;
+      offscreen.width = img.width;
+      //
+      octx.drawImage(img, 0, 0);
+      let data = octx.getImageData(0, 0, offscreen.width, offscreen.height);
+      img2.src = offscreen.toDataURL();
+      // document.body.append(offscreen)
+      // console.log(img2)
+      //ctx.drawImage(img2,0,0,offscreen.width,offscreen.height,0,0,canvas.width,canvas.height)
+      //ctx.putImageData(data,0,0)
     };
-    let url = URL.createObjectURL(e.target.files[0]);
-    img.src = url;
+    img.src = "chad.png";
+    img2.onload = () => {
+      drawCanvas();
+    };
+    // load chad,
+    // then have second image that you store the canvas as
+    canvas.height = cw;
+    canvas.width = ch;
 
-    // make the grid
-
-    // canvas events
     let last = { x: 0, y: 0 };
     let start;
     let moving;
@@ -78,37 +102,29 @@
       "touchstart",
       function(e) {
         var touches = e.touches[0];
-        if (markstate == "mark-on") {
-          // make mark function
-          storeMark(touches.clientX, touches.clientY);
-        } else {
-          start = {
-            x: touches.clientX * slow,
-            y: touches.clientY * slow
-          };
-        }
+        start = {
+          x: touches.clientX * slow,
+          y: touches.clientY * slow
+        };
+        editor.run(start.x,start.y)
       },
       false
     );
     canvas.addEventListener("touchmove", function(e) {
-      if (markstate != "mark-on") {
-        var touch = e.touches[0];
-        moving = {
-          x: touch.clientX * slow,
-          y: touch.clientY * slow
-        };
-        let delta = { x: start.x - moving.x, y: start.y - moving.y };
-        sx = last.x + delta.x;
-        sy = last.y + delta.y;
-      }
+      var touch = e.touches[0];
+      moving = {
+        x: touch.clientX * slow,
+        y: touch.clientY * slow
+      };
+      let delta = { x: start.x - moving.x, y: start.y - moving.y };
+      sx = last.x + delta.x;
+      sy = last.y + delta.y;
     });
     canvas.addEventListener("touchend", function(e) {
-      if (markstate != "mark-on") {
-        last = {
-          x: last.x + (start.x - moving.x),
-          y: last.y + (start.y - moving.y)
-        };
-      }
+      last = {
+        x: last.x + (start.x - moving.x),
+        y: last.y + (start.y - moving.y)
+      };
     });
     // Prevent scrolling when touching the canvas
     document.body.addEventListener(
@@ -138,130 +154,12 @@
       },
       { passive: false }
     );
-  };
-  let reset = () => {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    let conversion = img.width / canvas.width;
-    ctx.drawImage(
-      img,
-      0,
-      0,
-      img.width,
-      img.height,
-      0,
-      0,
-      canvas.width,
-      img.height / conversion
-    );
-    img2.src = canvas.toDataURL();
-  };
-  let storeMark = (x, y) => {
-    if (swap) {
-      canvas.width = img.width;
-      canvas.height = img.height;
-      let conversion = img.width / canvas.width;
-      ctx.drawImage(
-        img,
-        0,
-        0,
-        img.width,
-        img.height,
-        0,
-        0,
-        canvas.width,
-        img.height / conversion
-      );
-      swap = false;
-    } else {
-      ctx.fillStyle = "red";
-      ctx.fillRect(x, y, 5, 5);
-      img2.src = canvas.toDataURL();
-    }
-  };
-  let slowFunc = () => {
-    if (slow == 1) {
-      slow = 0.1;
-      slowstate = "super-slow-mo";
-    } else if (slow == 0.1) {
-      slow = 0.01;
-      slowstate = "fast-mo";
-    } else {
-      slow = 1;
-      slowstate = "slo-mo";
-    }
-  };
-  let markFunc = () => {
-    if (markstate == "mark-on") {
-      markstate = "mark-off";
-    } else {
-      markstate = "mark-on";
-      swap = true;
-    }
-  };
-
-  let func = () => {
-    if (ctx == undefined) return;
-
-    canvas.width = cw;
-    canvas.height = ch;
-    ctx.globalAlpha = GA;
-    ctx.drawImage(
-      img2,
-      sx,
-      sy,
-      img.width * zoom,
-      img.height * zoom,
-      0,
-      0,
-      (img.width * canvas.height) / img.height,
-      canvas.height
-    );
-  };
+  });
   $: if (GA) {
-    console.log(sx, sy, GA, zoom);
-    func();
-  }
-  let grid = () => {
-    if (ctx == undefined) return;
-    console.log("running grid");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    let conversion = img.width / canvas.width;
-    ctx.drawImage(
-      img,
-      0,
-      0,
-      img.width,
-      img.height,
-      0,
-      0,
-      canvas.width,
-      img.height / conversion
-    );
-    // makee the x grid
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "red";
-    for (let i = 0; i < gridNum; i++) {
-      ctx.beginPath();
-      ctx.moveTo(0, (canvas.height / gridNum) * i);
-      ctx.lineTo(canvas.width, (canvas.height / gridNum) * i);
-      ctx.closePath();
-      ctx.stroke();
-      ctx.beginPath();
-      ctx.moveTo((canvas.width / gridNum) * i, 0);
-      ctx.lineTo((canvas.width / gridNum) * i, canvas.height);
-      ctx.closePath();
-      ctx.stroke();
-    }
-    img2.src = canvas.toDataURL();
-  };
-  $: if (gridNum) {
-    console.log(gridNum);
-    grid();
+    console.log(sx, sy, zoom, GA);
+    drawCanvas();
   }
 </script>
-
 <style>
   canvas {
     position: absolute;
@@ -275,24 +173,15 @@
     background:white;
   }
 </style>
-
 <canvas bind:this={canvas} />
+
 <div id="holder">
-  <div id="fileupload">
-    <input type="file" on:change={handleFiles} />
-  </div>
+  <div id="fileupload" />
   <div id="zoom">
     <label for="">
       zoom
       <input type="text" min="0" max="1" bind:value={zoom} />
       <input type="range" min="0" max="1" step=".0001" bind:value={zoom} />
-    </label>
-  </div>
-  <div id="grid">
-    <label for="">
-      grid
-      <input type="text" min="5" max="20" bind:value={gridNum} />
-      <input type="range" min="5" max="20" step="1" bind:value={gridNum} />
     </label>
   </div>
   <div id="GA">
@@ -301,9 +190,6 @@
       <input type="text" min="0" max="1" bind:value={GA} />
       <input type="range" min="0" max="1" step=".001" bind:value={GA} />
     </label>
-  </div>
-  <div id="slowmo">
-    <button on:click={slowFunc}>{slowstate}</button>
   </div>
   <div id="mark">
     <button on:click={markFunc}>{markstate}</button>
